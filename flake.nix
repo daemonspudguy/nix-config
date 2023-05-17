c4efa39b0c4e2f5b514384f90cf88f01f0c4e9c4
Jacob Alexander Tice <jacob.a.tice@gmail.com>
2023-05-17 17:35:12 -0400

    update

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
       nixosConfigurations = {
         # FIXME replace with your hostname
         nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
+          specialArgs = { inherit inputs outputs self system; };
           modules = [
               ({ self, system, ... }: {
             environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [
